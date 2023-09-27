d6c5492c347354679b4a9df47daede06ab516ce3
Carlos J. Rosario <carlosjrosario@gmail.com>
2023-09-27 16:37:52 -0400

    se crea el archivo test.py

---
+++ tests.py
@@ -0,0 +1,8 @@
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "La suma no coincide"
+
+def test_multiply():
+    assert multiply(2, 3) == 6, "La multiplicación no coincide"
+
+def multiply(a, b):
+    return a * b
